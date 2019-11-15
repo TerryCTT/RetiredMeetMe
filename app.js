a5b74fd4beb2c60b015cfb1183d72a7c2d2bf91e
jan <rodriguez.janrichard@gmail.com>
2019-11-15 13:42:37 -0800

    Changing environment port

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ app.get('/testing', function(req,res){
 })
 
 var port = process.env.port || 3000;
-app.listen(port,process.env.IP,function(){
-  console.log("MeetMe.io has started");
-  console.log(port);
-});
+app.listen(process.env.PORT,process.env.IP, function(){
+    console.log("Server is Running, we up in this");
+})
