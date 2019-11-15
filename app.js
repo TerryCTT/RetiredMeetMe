0c08a3ec9070c64f178efc8a8339d6d1dec486a6
jan <rodriguez.janrichard@gmail.com>
2019-11-15 15:58:44 -0800

    Added adress logic and converting to coordinates

--- app.js
+++ app.js
@@ -11,17 +11,20 @@ app.get('/', function(req, res) {
 });
 
 users = [];
+
 io.on('connection', function(socket) {
    console.log('A user connected');
    socket.on('setUsername', function(data) {
 
-      if(users.indexOf(data) > -1) {
+      if(users.indexOf(data.name) > -1) {
          socket.emit('userExists', data + ' username is taken! Try some other username.');
       } else {
          users.push(data);
-         socket.emit('userSet', {username: data});
+				 console.log(users);
+         socket.emit('userSet', {username: data.name});
       }
    });
+
 
    socket.on('msg', function(data) {
       //Send message to everyone
