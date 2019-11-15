3d4434117f0294bbb95f8756d987c48d5d7d381f
jan <rodriguez.janrichard@gmail.com>
2019-11-15 12:16:27 -0800

    Starter room functionality

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-var express = require('express');
-var app = express();
+var express     = require('express');
+var app         = require('express')();
+var server      = require('http').createServer(app);
+var io          = require('socket.io')(server);
+var ejs         = require('ejs');
 
-app.set("view engine","ejs");
-app.use(express.static("public"));
 
+app.set('view engine', 'ejs');
+app.get('/', function(req, res) {
+    res.render('landing');
+});
 
-app.get('/', function(req,res){
-    res.render("landing");
-})
+io.on('connection', function(socket) {
+    console.log('a user connected');
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+      });
+});
 
-app.get('/testing', function(req,res){
-    res.render("meeting");
-})
-
-var port = process.env.port || 3000;
-app.listen(port,process.env.IP,function(){
-  console.log("MeetMe.io has started");
-  console.log(port);
+server.listen(process.env.PORT || 3000, function(){
+    console.log('app running');
 });
